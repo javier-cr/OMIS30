7d88ae2cfa8f23d07d86e2330cc37c5129604615
Javier Ramirez <javiercramirez@live.com>
2018-10-24 22:20:28 -0700

    Latest update

--- current_version.py
+++ current_version.py
@@ -39,7 +39,7 @@ cardvalues= ["A","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
 def value_of_card(card):
     if card[0] in cardvalues[9:12+1]: # if card is a 10 or J/Q/K
         return int(10)
-    elif card[0] in cardvalues[1:9]: # if card is a #
+    elif card[0] in cardvalues[1:9]: # if card is a # 2 thru 9, inclusive
         return int(card[0])
     elif card[0] == "A": # if card is a Ace
         #create value_hand list with total value of cards. If over 11 with ace in it, 
@@ -57,7 +57,6 @@ def value_of_card(card):
         elif value_hand == 0:
             ace_choice = input("You drew an Ace. Would you like to treat the Ace as a 1 or 11? ")
         else: return int(999999)
-
 
         # first card, and we need to keep going. Ask user if 1 or 11
         # not first card, user hand is 11 or less --> ask user if 1 or 11
