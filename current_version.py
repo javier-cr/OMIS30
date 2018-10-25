13b6e4b2f7e548d930ea2143b30314b7e967de69
Javier Ramirez <javiercramirez@live.com>
2018-10-24 23:12:14 -0700

    Update

--- current_version.py
+++ current_version.py
@@ -37,54 +37,21 @@ cardvalues= ["A","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
 
 #Function for providing integer value for cards dealt
 def value_of_card(card):
-    if card[0] == "10" or card[0]=="J" or card[0]=="Q" or card[0]=="K": # if card is a 10 or J/Q/K
+    #only reading first slice to determine value of the card
+    if card[:1] in ('J','Q','K','1'):
         return int(10)
-    elif card[0] in cardvalues[1:9]: # if card is a # 2 thru 9, inclusive
-        return int(card[0])
-    elif card[0] == "A": # if card is a Ace
-        #create value_hand list with total value of cards. If over 11 with ace in it, 
-        # then ace has to be a 1
-        if value_hand >= 11:
-            return int(1)
-        elif value_hand < 11:
-            ace_choice = input("You drew an Ace. Would you like to treat the Ace as a 1 or 11? ")
-            if ace_choice == '1':
+    elif card[:1] in ('2','3','4','5','6','7','8','9'):
+        return int(card[:1])
+    elif card[:1] == 'A':
+        print ("\n"+ str(card))
+        num = input("Would you like this Ace to be worth 1 or 11? ")
+        while num !='1' or num !='11':
+            if num == '1':
                 return int(1)
-            elif ace_choice == '11':
+            elif num == '11':
                 return int(11)
             else:
-                return int(999999)
-        elif value_hand == 0:
-            ace_choice = input("You drew an Ace. Would you like to treat the Ace as a 1 or 11? ")
-        else: return int(999999)
-
-        # first card, and we need to keep going. Ask user if 1 or 11
-        # not first card, user hand is 11 or less --> ask user if 1 or 11
-        # not first card, user hand is 12 or greater --> treat as 1
-
-        
-        
-        
-        
-        '''
-        if value_hand>11: 
-            return int(1)
-        else:
-            ace_value= input("Would you like to treat the " + str(card) +\
-             " as a 1 or 11? \n")
-            while ace_value !="1" or ace_value !="11":    #input validation
-                if ace_value== "1" or ace_value== "11":
-                    return int(ace_value)
-                elif ace_value== "one":
-                    return int(1)
-                elif ace_value== "eleven":
-                    return int(11)
-                else:
-                    ace_value== str(input("Please enter a 1 or 11 as the value\
-                     for your " + str(card)+ "? \n"))
-    else:
-        return int(999999)   
-    '''        
+                num = input("Would you like this Ace to be worth 1 or 11? ")
 
 #give a new card and remove card from original list                 
 def new_card(deck_of_six):   
