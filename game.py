dd7215a6bb0db92514e35b16410f67801da385f8
Javier Ramirez <javiercramirez@live.com>
2018-12-02 18:07:56 -0800

    Added intro, case selection, showing user cases they picked

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
-# FROM CHRIS:
 import random
+import time
+
+cases_Remaining = []
+cases_Remaining.extend((4,5,6,7,8)) #remove this line
+cases_Chosen = []
 
 class dealOrNoDeal():
     briefcases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,\
@@ -18,16 +22,52 @@ class dealOrNoDeal():
         self.priceLeft = sorted(self.listPrice)
         random.shuffle(self.briefcases)
         self.briefcaseWithValue = dict(zip(self.briefcases, self.listPrice))
-
-    
-
-
-
     
     def finalCase(self):
         print("You have case number " + cases_Remaining +" and your original case number of "+ user_Case + " left.")
-        user_final_choice= int(input("Which case number would you like to pick? " + user_Case + " or " + cases_Remaining " ?"))
+        user_final_choice= int(input("Which case number would you like to pick? " + user_Case + " or " + cases_Remaining + " ?"))
         print("You have chosen case number " + user_final_choice + ".")
         print("You win " + self.briefcaseWithValue[self.user_final_choice] + " dollars!")
         print("You have reached the end of the game. Thanks for playing!")
+
+# Keep running game while run_game = True. If we want to end the game,
+# we set run_game = False.
+keep_running = True
+while keep_running:
+    
+    # INTRO
+    print('Welcome to Deal or No Deal!\nYou may choose a case numbered 1-26.')
+    
+    user_Case = input('\nWhich case would you like to select to be yours? Case #')
+    cases_Remaining.remove(user_Case) # remove from available cases
+    cases_Chosen.append(user_Case) # add to cases chosen
+    cases_this_round = 6 # how many cases they'll open this round. start at 6
+
+    print('\nGreat! You have selected your case.\nNow, select ' +\
+    str(cases_this_round) + ' cases to open this round.')
+
+    # Ask user which cases to open
+    cases_opened = []
+    for i in range(1, cases_this_round+1):
+        current_num = input('Which case should be Case #' + str(i) + '? Case #')
+        cases_opened.append(current_num)
+        
+        if i == cases_this_round+1:
+            cases_this_round -= 1 # so that user will open 1 less case next time
+        else:
+            pass
+
+    # Show user values of cases they opened
+    print('\nYour personal case is Case #' + str(user_Case) + ". Let's open the \
+cases you picked.\nThe cases you picked had the following values \
+inside of them: ")
+    
+    for j in range (0, len(cases_opened)):
+        time.sleep(2) #delay for 2 seconds
+        print ('Case #' + (str(cases_opened[j]) + ' had X amount inside of it.'))
+
+    # Banker calls - CHRIS
+
+
+    keep_running = False # end game?
     
