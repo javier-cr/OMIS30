e934c60c52b4945a36e56aac8e3ef623f72a290a
ChrisMartin14 <ctmartin@scu.edu>
2018-12-03 21:28:16 -0800

    unindent

--- game.py
+++ game.py
@@ -60,9 +60,9 @@ while keep_running:
             pass
 
     # Show user values of cases they opened
-    print('\nYour personal case is Case #' + str(user_case) + ". Let's open the \
-    cases you picked.\nThe cases you picked had the following values \
-    inside of them: ")
+    print('\nYour personal case is Case #' + str(user_case) + ". Let's open the\
+cases you picked.\nThe cases you picked had the following values\
+inside of them: ")
     
     for j in range (0, len(cases_opened)):
         time.sleep(2) #delay for 2 seconds
