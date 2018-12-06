56a1a70678b7ff1dd115c90aa3e6825370a77561
ChrisMartin14 <ctmartin@scu.edu>
2018-12-06 10:42:36 -0800

    add

--- game.py
+++ game.py
@@ -115,7 +115,7 @@ def finalCase(): #left for tomorrow
 def finalDecision():
     final_choice = input('You have made it to the last round. You must \
 either open your personal case or the only one left.\
-Your personal case is ' + str(user_case_num) + ". \
+Your personal case is case #' + str(user_case_num) + ". \
 The only case left is " + str(combo.keys())+ ". \
 Enter 'mine' to keep your case or 'switch' to choose the final case.\n")
     if final_choice == 'mine' or 'Mine':
